7ed9fcba0a0dee732ce21cad80fd5b0d719a1b5b
Symphony0998 <568603742@qq.com>
2016-07-20 15:30:51 +0800

    Bug Fixed and More specified name
    add class number and student name in file name

--- Spider_'fifedu'_beta.py
+++ Spider_'fifedu'_beta.py
@@ -4,7 +4,7 @@ import re
 import requests
 from bs4 import BeautifulSoup
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36',
-          'Cookie': 'JSESSIONID=66AADBAC9ED67801FE868AAAF8C51175-n1; _webim_cookie_adb9f412a82e40cfb88c8b11ff03b640=%7B%22o%22%3Afalse%2C%22s%22%3A%22available%22%7D; sel_nav=sy; CNZZDATA1253891395=1568598399-1468215028-http%253A%252F%252Fbjut.fifedu.com%252F%7C1468742975'}
+          'Cookie': 'JSESSIONID=F20490CB26A2282FFEB3EF143ACAB86F-n1; CNZZDATA1253891395=1568598399-1468215028-http%253A%252F%252Fbjut.fifedu.com%252F%7C1468737423; sel_nav=sy'}
 class_url = 'http://bjut.fifedu.com/iplat/bp/student/class/studentList?classId=2811000026000002'
 userdetail_url = 'http://bjut.fifedu.com/iplat/bp/teacher/class/userDetail'
 class_number_list = []
@@ -32,13 +32,15 @@ for i in range(27,159):
             soup = BeautifulSoup(response.read())
             pattern_for_student_pictureurl = re.compile('.*?"note":"","avator":"(.*?)".*?')
             student_picture_url = 'http://bjut.fifedu.com/iplat/' + re.findall(pattern_for_student_pictureurl, str(soup))[0]
-            picture = urllib.request.urlopen(student_picture_url).read()
-            pattern_for_student_idCardNo = re.compile(
-                'http://bjut.fifedu.com/iplat//upload/bjut/2.*?/(.*?).jpg')
+            pattern_for_userName = re.compile('.*?"userName":"(.*?)","gender":".*?')
+            student_userName = re.findall(pattern_for_userName, str(soup))[0]
             try:
+                picture = urllib.request.urlopen(student_picture_url).read()
+                pattern_for_student_idCardNo = re.compile(
+                    'http://bjut.fifedu.com/iplat//upload/bjut/2.*?/(.*?).jpg')
                 student_idCardNo = re.findall(pattern_for_student_idCardNo, student_picture_url)[0]
                 print('当前学生：' + student_idCardNo + '成功')
-                file = open('d:\spider_picture/' + student_idCardNo + '.jpg', 'wb')
+                file = open('d:\spider_picture/' + str(class_number[0]) + student_userName + student_idCardNo + '.jpg', 'wb')
                 file.write(picture)
                 file.close()
             except:
